abde2b78716f527f1d97c235f0d7ec83294ff3a2
gumiranda <imartytk@hotmail.com>
2016-05-21 23:53:35 -0300

    Update exercicio1.pl

--- exercicio1.pl
+++ exercicio1.pl
@@ -1,3 +1,6 @@
-acomoda([H|T],L):-acomoda(H,L1),acomoda(T,L2),append(L1,L2,L).
+concat([],L,L).
+concat([H|T],L2,[H|T3]):-concat(T,L2,T3).
+
+acomoda([H|T],L):-acomoda(H,L1),acomoda(T,L2),concat(L1,L2,L).
 acomoda([],[]).
 acomoda(H,[H]):-H\=[],H\=[_|_].
